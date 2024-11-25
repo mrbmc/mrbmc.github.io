25821f54d4915d55b9fb029d2611ab5bf8cd9709
Brian McConnell <brian@mrbmc.com>
2024-11-25 13:14:59 -0500

    style refinemenets

--- src/_js/portfolio.js
+++ src/_js/portfolio.js
@@ -8,15 +8,41 @@ function wrapElement (el, wrapper) {
     wrapper.appendChild(el);
 }
 
+function initMosaics() {
+  var mosaics = document.getElementsByClassName('mosaic');
+  for (const mos of mosaics) {
+    var imgs = mos.getElementsByTagName('img');
+    console.log('mosaics',imgs)
+    for (const img of imgs) {
+      img.addEventListener('click',function(e){
+        var newImg = document.createElement('img');
+            newImg.setAttribute('src',this.src);
+
+        document.getElementById('lightbox').innerHTML = "";
+        document.getElementById('lightbox').append(newImg);
+        document.getElementById('lightbox').classList.add('in');
+      })
+    }
+  }
+  document.getElementById('lightbox').addEventListener('click',()=>{
+    document.getElementById('lightbox').classList.remove('in');
+  })
+}
 
 window.addEventListener('keydown', function(e) {
     console.log('onkeydown',e);
+
+    switch(e.keyCode) {
+        case 27://escape
+          document.getElementById('lightbox').classList.remove("in");
+          break;
+        default:
+    }
     galleryKeyPress(e);
 });
 window.addEventListener('popstate',function(e){
     galleryPopstate(e);
 });
-
 
 window.addEventListener('load', function(e) {
     console.log('project.window.load',e);
@@ -30,7 +56,9 @@ window.addEventListener('load', function(e) {
         });
     });
 
+    initMosaics();
 
     galleryLoad(e);
 });
+
 
